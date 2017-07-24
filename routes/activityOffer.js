d4340867e3a629dc11ffa6803673c08964b65342
xuehengli2012 <xuehengli2012@gmail.com>
2017-07-24 16:25:46 +0800

    modify code

--- routes/activityOffer.js
+++ routes/activityOffer.js
@@ -163,10 +163,10 @@ exports.execute = function( req, res ) {
 			if (response.statusCode == 201) {
 				data = JSON.parse(data);
 				console.log('onEND PushResponse:', response.statusCode, data);
-				res.send(200, {"pushId": data.id});
+				res.send(200, {"pushId": data.id, "StatusCode": 200, "SubscriberKey": SubscriberKey});
 			} else {
 				console.log('onEND fail:', response.statusCode);
-				res.send(response.statusCode);
+				res.send(response.statusCode, {"StatusCode": response.statusCode, "SubscriberKey": SubscriberKey});
 			}		
 		});								
 	});
