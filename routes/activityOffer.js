d686bf671469b2c57f0d8684992a5b1ed9331494
xuehengli2012 <xuehengli2012@gmail.com>
2017-07-28 12:51:54 +0800

    modify code

--- routes/activityOffer.js
+++ routes/activityOffer.js
@@ -108,7 +108,7 @@ function remote_call(req,res) {
 	
 	// Prepare post data for remote API
 	//var pushInfo = JSON.stringify([{"muid": muid, "msg": pushMessageText}]);
-	var pushInfo = JSON.stringify([{"muid":"9161521889284","msg":" ","url":"","badge":1}]);
+	var pushInfo = JSON.stringify([{"muid":"9161521889284","msg":" _","url":"","badge":1}]);
 	console.log('pushInfo=', pushInfo);
 
 	//var endpoint = "https://jsonplaceholder.typicode.com/posts";
